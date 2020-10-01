fc75939b59decc73a34219834593f6b7a991c34b
dungnguyen1709 <56040352+dungnguyen1709@users.noreply.github.com>
2020-10-01 17:19:38 +0700

    commit

--- src/main/java/com/codegym/config/CustomSuccessHandler.java
+++ src/main/java/com/codegym/config/CustomSuccessHandler.java
@@ -47,9 +47,8 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
             roles.add(a.getAuthority());
         }
 
-        if (isDba(roles)) {
-            url = "/dba";
-        } else if (isAdmin(roles)) {
+
+        if (isAdmin(roles)) {
             url = "/admin";
         } else if (isUser(roles)) {
             url = "/user";
@@ -74,12 +73,6 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         return false;
     }
 
-    private boolean isDba(List<String> roles) {
-        if (roles.contains("ROLE_DBA")) {
-            return true;
-        }
-        return false;
-    }
 
     public void setRedirectStrategy(RedirectStrategy redirectStrategy) {
         this.redirectStrategy = redirectStrategy;
--- src/main/java/com/codegym/controller/SecurityController.java
+++ src/main/java/com/codegym/controller/SecurityController.java
@@ -2,8 +2,11 @@ package com.codegym.controller;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,16 +17,33 @@ import javax.servlet.http.HttpServletResponse;
 @Controller
 public class SecurityController {
 
-    @GetMapping("/")
-    public String getHomePage() {
+    private String getPrincipal(){
+        String userName = null;
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        if (principal instanceof UserDetails) {
+            userName = ((UserDetails)principal).getUsername();
+        } else {
+            userName = principal.toString();
+        }
+        return userName;
+    }
+
+    @GetMapping(value = {"/", "/home"})
+    public String Homepage(Model model){
+        model.addAttribute("user", getPrincipal());
         return "home";
     }
-    @GetMapping("/admin")
-    public String getAdminPage() {
+
+    @RequestMapping(value = "/admin", method = RequestMethod.GET)
+    public String adminPage(ModelMap model) {
+        model.addAttribute("user", getPrincipal());
         return "admin";
     }
-    @GetMapping("/user")
-    public String getUserPage() {
+
+    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    public String userPage(ModelMap model) {
+        model.addAttribute("user", getPrincipal());
         return "user";
     }
 
@@ -36,4 +56,9 @@ public class SecurityController {
         return "redirect:/login?logout";
     }
 
+    @RequestMapping(value = "/Access_Denied", method = RequestMethod.GET)
+    public String accessDeniedPage(ModelMap model) {
+        model.addAttribute("user", getPrincipal());
+        return "accessDenied";
+    }
 }
