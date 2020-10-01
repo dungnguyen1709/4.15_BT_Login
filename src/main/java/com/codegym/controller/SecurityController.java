132858cc46052b599f55b8c26def7816c8333b33
dungnguyen1709 <56040352+dungnguyen1709@users.noreply.github.com>
2020-10-01 17:02:09 +0700

    commit

--- src/main/java/com/codegym/controller/SecurityController.java
+++ src/main/java/com/codegym/controller/SecurityController.java
@@ -1,7 +1,15 @@
 package com.codegym.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class SecurityController {
@@ -17,6 +25,15 @@ public class SecurityController {
     @GetMapping("/user")
     public String getUserPage() {
         return "user";
+    }
+
+    @RequestMapping(value="/logout", method = RequestMethod.GET)
+    public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/login?logout";
     }
 
 }
